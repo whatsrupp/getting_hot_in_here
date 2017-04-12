e26bb2f06fd9536e676b8c05ababb191b03540a0
Nick Rupp <nickrupp95@gmail.com>
2017-04-12 11:15:43 +0100

    Add temperature up and down functionality

--- Thermostat/spec/thermostatSpec.js
+++ Thermostat/spec/thermostatSpec.js
@@ -1,4 +1,4 @@
-console.log("Thermostat Spec Required")
+'use strict';
 describe('Thermostat', function(){
   var thermostat;
 
@@ -7,9 +7,43 @@ describe('Thermostat', function(){
   });
 
   describe('properties of thermostat', function(){
+
     it('initializes with a temperature of 20', function(){
       expect(thermostat.temperature).toEqual(20);
     });
+
+    it('initializes in powersaving mode', function(){
+      expect(thermostat.isPowerSaverOn).toEqual(true);
+    });
+
+    it('initalizes with a current max temperature of 25', function(){
+      expect(thermostat.maxTemperature).toEqual(25);
+    });
+
+  });
+
+  describe('Context: User wants to change temperature', function(){
+
+    describe('#temperatureUp', function(){
+
+      it('increases temperature when user calls temperature up', function(){
+        var startingTemp = thermostat.temperature;
+        thermostat.temperatureUp();
+        var endingTemp = thermostat.temperature;
+        expect(endingTemp-startingTemp).toEqual(1);
+      });
+    });
+
+    describe('#temperatureDown', function(){
+      
+      it('decreases the temperature when user calls temperatureDown', function(){
+        var startingTemp = thermostat.temperature;
+        thermostat.temperatureDown();
+        var endingTemp = thermostat.temperature;
+        expect(startingTemp-endingTemp).toEqual(1);
+      });
+    });
+
   });
 
 });
--- Thermostat/src/thermostat.js
+++ Thermostat/src/thermostat.js
@@ -1,3 +1,13 @@
 function Thermostat (){
   this.temperature = 20;
+  this.isPowerSaverOn = true;
+  this.maxTemperature = 25;
 };
+
+Thermostat.prototype.temperatureUp = function(){
+  this.temperature++;
+}
+
+Thermostat.prototype.temperatureDown = function(){
+  this.temperature--;
+}
